478eea71876e1d6573a28b4e35c65be8df9ddd8d
Kaio Da Dalt <kaiodadalt@gmail.com>
2024-04-11 19:58:41 -0300

    Feat: added the required API methods.

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Http\Controllers\BankingController;
+use Illuminate\Support\Facades\Route;
 
+Route::post('/reset', [BankingController::class, 'reset']);
+
+Route::get('/balance', [BankingController::class, 'getBalance']);
+Route::post('/event', [BankingController::class, 'handleEvent']);
